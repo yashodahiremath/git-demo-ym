b4c0ef7e8691c652ebc27847f4ef50ade479bd34
yashodahiremath <yhiremath07@gmail.com>
2025-05-13 20:44:25 +0530

    Add files via upload

---
+++ Script1.sh
@@ -0,0 +1,5 @@
+echo "We are trying this practical from linux EC2"
+echo "I am from Young minds"
+echo "We are from batch-27 and learning DevOps"
+echo "We are learning Git and GitHub"
+echo "How are you?"
